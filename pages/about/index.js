588dd0b28950e5af9fb871f771da26bedaf9afbf
Kevin Chifamba <63073697+puureya2@users.noreply.github.com>
2025-06-19 13:56:27 +0800

    more change

--- pages/about/index.js
+++ pages/about/index.js
@@ -311,10 +311,10 @@ const About = () => {
                   <div className='flex gap-x-4'>
 
                     {/* icons */}
-                    {item.icons?.map((icon, itemIndex) => {
+                    {item.icons?.map((icon, iconIndex) => {
                       
                       return (
-                      <div key={itemIndex} className='text-2xl text-white'>
+                      <div key={iconIndex} className='text-2xl text-white'>
                         {icon}
                       </div>
                     );
